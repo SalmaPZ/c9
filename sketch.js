d5bb1c95397c5d669d2b0c98e9ce15b85e7689d0
SalmaPZ <47847444+SalmaPZ@users.noreply.github.com>
2021-09-14 18:35:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+var a
+function setup() {
+  createCanvas(400, 400);
+ // a = createSprite(100,100,50,50);
+}
+
+
+
+function draw() {
+  background("red");
+
+  textSize(25)
+  fill("yellow")
+  text("Hello!!",100,100)
+ // drawSprites();
+}
